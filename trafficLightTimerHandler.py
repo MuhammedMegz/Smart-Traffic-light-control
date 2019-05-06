8d076a7241879cf32a2082388c6c2afc14f321f5
Eslam Adel Abuelseaoud <eslam.abuelseaoud@gmail.com>
2019-05-06 23:26:00 +0200

    Update trafficLightTimerHandler.py
    main&test

--- trafficLightTimerHandler.py
+++ trafficLightTimerHandler.py
@@ -25,3 +25,47 @@ def waited_time(q,x):
     elif waited_time > 90 :
         waited_time=90
     return int(waited_time-(waited_time%5))
+
+while 1:
+    #read from socket.io
+    '''
+    right_traffic_num=x
+    forward_traffic_num=y
+    
+    '''
+    right_traffic_num=int(input("right"))
+    if right_traffic_num==122 :
+        break
+    forward_traffic_num=int(input("forward"))
+    
+   
+        
+    if turn==0:
+        # the turn is on right
+        time=waited_time(right_traffic_num,forward_traffic_num)
+        right_traffic=1
+        forward_traffic=0
+      
+    if turn==1:
+        # the turn is on forward
+        time=waited_time(forward_traffic_num,right_traffic_num)
+        right_traffic=0
+        forward_traffic=1
+    print(str(right_traffic)+"    "+str(forward_traffic))
+    print(time)
+    
+    print("----------------------------------------")
+    
+            
+    turn=(turn+1)%2
+    
+    
+    time.sleep(waited_time)
+    
+        
+        
+        
+    
+    
+
+
