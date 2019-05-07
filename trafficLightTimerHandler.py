4619e4a560aa4feef4fdcd051535e9a292d4d794
Islam El-Mahdy <islamelmahdy96@gmail.com>
2019-05-07 04:03:41 +0200

    algorithm modification

--- trafficLightTimerHandler.py
+++ trafficLightTimerHandler.py
@@ -1,71 +1,33 @@
-import time
-
-
-
-right_traffic_num=0
-forward_traffic_num=0
-right_traffic=0
-forward_traffic=0
-min_waited_time=10
-max_waited_time=90
-turn=0
-
-
-
+min_waited_time = 10
+max_waited_time = 60
 
 def ratio (q,x):
     return q/x
+
+
 def waited_time(q,x):
     if q==0 and x==0:
-        return 10
+        return min_waited_time
     ratio=q/(q+x)
-    waited_time=ratio*90
-    if waited_time<10 :
-        waited_time=10
-    elif waited_time > 90 :
-        waited_time=90
+    waited_time=ratio*max_waited_time
+    if waited_time < min_waited_time :
+        waited_time = min_waited_time
+    elif waited_time > max_waited_time :
+        waited_time = max_waited_time
     return int(waited_time-(waited_time%5))
 
-while 1:
-    #read from socket.io
-    '''
-    right_traffic_num=x
-    forward_traffic_num=y
-    
-    '''
-    right_traffic_num=int(input("right"))
-    if right_traffic_num==122 :
-        break
-    forward_traffic_num=int(input("forward"))
-    
-   
+
+def calcTime(forward_traffic_num, right_traffic_num, turn):   
         
-    if turn==0:
-        # the turn is on right
-        time=waited_time(right_traffic_num,forward_traffic_num)
-        right_traffic=1
-        forward_traffic=0
+    if turn=="EW":
+        # the turn is on EW
+        if right_traffic_num >= forward_traffic_num:
+            return "no change"
+        return waited_time(right_traffic_num,forward_traffic_num)
+
       
-    if turn==1:
-        # the turn is on forward
-        time=waited_time(forward_traffic_num,right_traffic_num)
-        right_traffic=0
-        forward_traffic=1
-    print(str(right_traffic)+"    "+str(forward_traffic))
-    print(time)
-    
-    print("----------------------------------------")
-    
-            
-    turn=(turn+1)%2
-    
-    
-    time.sleep(waited_time)
-    
-        
-        
-        
-    
-    
-
-
+    if turn=="NS":
+        # the turn is on NS
+        if forward_traffic_num >= right_traffic_num:
+            return "no change"
+        return waited_time(forward_traffic_num,right_traffic_num)
