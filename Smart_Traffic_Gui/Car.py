52301cb968b991b2356a35bc1f6cbf1f54445166
Mohamed Elattar <30553162+mohamedel3attar@users.noreply.github.com>
2019-05-07 04:27:18 +0200

    Update Car.py

--- Smart_Traffic_Gui/Car.py
+++ Smart_Traffic_Gui/Car.py
@@ -2,12 +2,17 @@ from tkinter import *
 
 class Car:
     def __init__(self, canvas, startPos, orientaion):
-        self.carImg = PhotoImage(file="photos/car.png")
-        self.startPos = startPos
-        self.canvas = canvas
-        self.carID = self.canvas.create_image(startPos[0], startPos[1], anchor=NE, image=self.carImg, state=HIDDEN)
-        self.isMoving = False
-        self.orientaion = orientaion
+      if orientaion == "NS":
+        self.carImg = PhotoImage(file="photos/carNS.png")
+        self.step = [0, -10]
+      elif orientaion == "EW":
+        self.carImg = PhotoImage(file="photos/carEW.png")
+        self.step = [-10, 0]
+      self.startPos = startPos
+      self.canvas = canvas
+      self.carID = self.canvas.create_image(startPos[0], startPos[1], anchor=CENTER, image=self.carImg, state=HIDDEN)
+      self.isMoving = False
+      self.orientaion = orientaion
 
     def move(self):
       self.isMoving = True
@@ -18,9 +23,11 @@ class Car:
       self.canvas.itemconfig(self.carID, state=HIDDEN)
 
     def moveLoop(self):
-      self.canvas.move(self.carID, -50, 0)
+      self.canvas.move(self.carID, self.step[0], self.step[1])
       if self.canvas.coords(self.carID)[0] < 0:
         self.canvas.move(self.carID, self.startPos[0], 0)
+      elif self.canvas.coords(self.carID)[1] < 0:
+        self.canvas.move(self.carID, 0, self.startPos[1])
       if self.isMoving:
         self.canvas.itemconfig(self.carID, state=NORMAL)
-        self.canvas.after(50, self.moveLoop)
+        self.canvas.after(100, self.moveLoop)
